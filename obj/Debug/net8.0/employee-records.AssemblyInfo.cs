c5228ad31d304310147f9d7305f94b336d89a725
KRISHNA-PRAVESH <krishnapravesh.hj@gmail.com>
2024-02-17 15:58:35 +0530

    added git ignore

--- obj/Debug/net8.0/employee-records.AssemblyInfo.cs
+++ obj/Debug/net8.0/employee-records.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("employee-records")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+213b3244352a7bc699c8969177d66fa000e1e43f")]
 [assembly: System.Reflection.AssemblyProductAttribute("employee-records")]
 [assembly: System.Reflection.AssemblyTitleAttribute("employee-records")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
